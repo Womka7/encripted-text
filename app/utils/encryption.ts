bd73196375c6c845a6ea9a8cd5a21089a6f45a91
Karina P <karina_pineda97@hotmail.com>
2024-08-25 05:54:34 -0500

    feat logic for vowels encriptation

---
+++ app/utils/encryption.ts
@@ -0,0 +1,17 @@
+export const encrypt = (text: string): string => {
+    return text
+      .replace(/e/g, 'enter')
+      .replace(/i/g, 'imes')
+      .replace(/a/g, 'ai')
+      .replace(/o/g, 'ober')
+      .replace(/u/g, 'ufat');
+  };
+  
+  export const decrypt = (text: string): string => {
+    return text
+      .replace(/enter/g, 'e')
+      .replace(/imes/g, 'i')
+      .replace(/ai/g, 'a')
+      .replace(/ober/g, 'o')
+      .replace(/ufat/g, 'u');
+  };
